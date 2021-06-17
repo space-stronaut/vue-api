9774097563f958737a62c49850f63d4b06096d4b
Spacestronaut <Abelr6099@gmail.com>
2021-06-17 13:13:54 +0700

    first vue api

---
+++ src/components/Data.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul 
+    :for="message in messages"
+    >
+        <li>{{message.name}}</li>
+    </ul>    
+</template>
+
+<script>
+export default {
+  name: 'Data',
+  data() {
+      return{
+          messages : []
+      }
+  },
+  mounted(){
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(json => (this.messages = json))
+  }
+}
+</script>
